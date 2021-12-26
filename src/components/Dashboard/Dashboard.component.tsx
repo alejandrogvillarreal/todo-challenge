d5a1001744f856594d314d6be4d4a21316f990f6
alejandrogvillarreal <alejandrogvillarreal@gmail.com>
2021-12-26 18:32:10 -0300

    todo challenge

--- src/components/Dashboard/Dashboard.component.tsx
+++ src/components/Dashboard/Dashboard.component.tsx
@@ -5,44 +5,17 @@ import DashboardContent from "./Dashboard.content";
 // types
 import { Task } from "../../types/task";
 
-const mock: Task[] = [
-  {
-    id: 0,
-    priority: { id: 1, name: "alta" },
-    state: { id: 1, name: "nueva" },
-    title: "Diseñar banner",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum reiciendis inventore dignissimos nemo laborum doloremque ratione laudantium atque quisquam earum esse excepturi eaque illo quas harum, omnis ipsum repellendus deserunt.",
-  },
-  {
-    id: 1,
-    priority: { id: 2, name: "media" },
-    state: { id: 1, name: "nueva" },
-    title: "Diseñar landig",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum reiciendis inventore dignissimos nemo laborum doloremque ratione laudantium atque quisquam earum esse excepturi eaque illo quas harum, omnis ipsum repellendus deserunt.",
-  },
-  {
-    id: 2,
-    priority: { id: 1, name: "alta" },
-    state: { id: 3, name: "finalizada" },
-    title: "Diseñar formulario",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum reiciendis inventore dignissimos nemo laborum doloremque ratione laudantium atque quisquam earum esse excepturi eaque illo quas harum, omnis ipsum repellendus deserunt.",
-  },
-];
-
 interface Props {}
 
 export default (props: Props) => {
   const [openTask, setOpenTask] = useState<any>(false);
-  const [tasks, setTasks] = useState<Task[]>(mock);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [taskSelected, setTaskSelected] = useState<Task | null>(null);
 
   const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
 
-  // esto lo uso para crear las tareas con != ids
-  const [ids, setIds] = useState<number>(2);
+  // use this to create tasks with != ids
+  const [ids, setIds] = useState<number>(0);
 
   const editTask = (task: Task) => {
     const foundIndex = tasks.findIndex((t) => t.id === task.id);
